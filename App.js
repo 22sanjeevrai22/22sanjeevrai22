f9c8ebb212885a83801552a385ddd3d36a0096cf
22sanjeevrai22 <87857906+22sanjeevrai22@users.noreply.github.com>
2022-11-21 21:13:14 +0545

    This is the CURD

--- App.js
+++ App.js
@@ -1,109 +1,158 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react';
 
 function App() {
-
-  return <LoginPage/>;
+                                        
+return <Todos/>;
 
 }
 
-function LoginPage(){
-  const [pleaseLogin, setPleaselogin] = useState('');
-  
+function Todos(){
 
-  const setMessage = () => { 
-    setPleaselogin((_) => 'Please loginnnnnnn')
-}
+     const [todolist, setTodoList] = useState([])
+    const fetchData = async () => {
+        const data = await fetch("https://jsonplaceholder.typicode.com/todos")
+        const jsonData = await data.json()
+        setTodoList(jsonData)
+    }
 
-  return (
-
-    <div className='container'>
-
-    <LoginTitle please = {pleaseLogin}/>
-    <AddUsernamePassword/>
+    useEffect(() => {
+        fetchData()
+    }, [])
 
 
-    </div>
+    const onAddTodo = (todo) => {
+        console.log('Parent todo', todo)
+        setTodoList((prev)=>[todo, ...prev])
 
-  )
-}
+    }
 
- 
-
-
-function LoginTitle({please}){
-  
-
-  return(
-    <>
-    <h2>Login-Page</h2>
-    <h3>{please}</h3>
-    </>
-  )
-  }
-
-
-
-function AddUsernamePassword(){
-
-  const[username, setUsername] = useState('');
-  const[password, setPassword] = useState('');
-
-  const handleUserChange = (event)=>{
-    // console.log(event)
-    const usernameValue = event.target.value
-    setUsername((_) => usernameValue)
-  }
-
-  const handlePasswordChange = (event)=>{
-    // console.log(event)
-    const usernameValue = event.target.value
-    setPassword((_) => usernameValue)
-  }
-
-  const handleSubmitButton = () => {
-    if(username && password){
-      if(username === 'admin' && password === 'admin'){
-        alert('Successful Login')
-      }
-      else{
-        alert('Failed Login')
-      }
+    const onCancel = () => {
+        setTodoList((_) => [])
     }
     
-  }
+    const deleteTodo = (index) => {
+        // const updatedTodos = todolist.filter((todo, tid) => {
+        //     return tid !== index;
+        // } ) 
+        // setTodoList((_) => updatedTodos)
+        console.log(index)
+        console.log('why does this go in a loop')
 
+        //Another method
+        // let reducedTodo = [...todolist]
+        // reducedTodo.splice(index)
+        // setTodoList(reducedTodo)
 
-
-  //  useEffect(() => {
-  //   LoginPage.setMessage();
-  
-
-  // }, [])
-
-  
-
-  return(
-    <>
-    <div className='user'>
-
-      <input type='text' onChange={handleUserChange} placeholder='Username'></input>
-    </div>
-  
-    <div className='password'>
-      <input type='password' onChange={handlePasswordChange} placeholder='Password'></input>
-    </div>
+    }
     
-    <div>
-      <button type='submit' onClick={handleSubmitButton}>Login</button>
-    </div>
 
-    </>
+   
+return(
 
-  )
+        <div className='todo-card'>
+
+        <TodoTitle/>
+        
+        <AddTodo onAddTodo = {onAddTodo}/>
+        
+        {todolist.length <= 0 ? 'Loading' : <TodoList todos = {todolist} deleteTodo={deleteTodo}/>}
+    
+
+        <TodoFooter onCancel = {onCancel}/>
+
+        </div>    
+);
+
+}
+
+
+function TodoTitle(){
+    return(
+        <div className='title'>
+        <h1>To-Do-List</h1>
+        </div>
+    )
+}
+
+function AddTodo({onAddTodo}){
+
+    const[todo, setTodo] = useState('');
+    const handleTodo =(event)=>{
+        const todoValue = event.target.value;
+
+    setTodo((_) => todoValue)
+    }
+
+    const handleTodoAdd = () =>{
+        onAddTodo(todo)
+        setTodo((prev)=>'')
+    }
+    
+    
+    return(
+        <div className='input'>
+            <input type='text' placeholder='What will you do next?' onChange={handleTodo} value={todo}></input>
+            <button onClick = {handleTodoAdd}>+</button>
+        </div>
+    )
+}
+
+function TodoList({todos, deleteTodo}){
+
+  const handleDeleteTodo = (index) => {
+    deleteTodo(index)
+
+    }
+    
+   
+    return(
+        <div id='one'>
+            {
+            todos.map((todo) => (
+                <>
+                <AtomicTodoList todo={todo} key ={todo.id}/>
+                <button onClick={handleDeleteTodo(todo.id)} className='btn'>-</button>
+                
+                
+                </>
+             )) 
+            }
+        </div>
+    );
+}
+
+//Making atomic Components
+function AtomicTodoList({todo}){
+  
+    
+    return(
+        <div className ='oneTodo' key={todo.id}>{todo.title}</div>
+
+    )
+}
+
+
+function TodoFooter({onCancel}){
+
+    return(
+        <div className='footer'>
+            <div className='pending-task' id='one'>
+                You have 3 pending task.
+            </div>
+
+            
+            <div>
+                <button className='btn' onClick={onCancel}>Clear all</button>
+            </div>
+
+        </div>
+    )
 }
 
 
 
 export default App;
+
+ 
